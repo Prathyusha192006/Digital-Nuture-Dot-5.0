048a338dc26978bfc4ab52ce0f49d86f85c1d991
Prathyusha chaluvadi <231fa04e60@gmail.com>
2026-06-29 11:54:50 +0530

    Create Exercise1_RankingAndWindowfunctions.sql

---
+++ Deepskilling/Advance SQL/1.SQL Exercise - Advanced concepts/Exercise1_RankingAndWindowfunctions.sql
@@ -0,0 +1,24 @@
+[10:08 pm, 20/06/2026] Prathyusha✨: ex-1
+[10:10 pm, 20/06/2026] Prathyusha✨: CREATE DATABASE IF NOT EXISTS CognizantSQL;
+USE CognizantSQL;
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    EmployeeName VARCHAR(50),
+    Department VARCHAR(50),
+    Salary INT
+);
+INSERT INTO Employees VALUES
+(1,'Sai','IT',50000),
+(2,'Prathyusha','IT',60000),
+(3,'Nandhu','HR',45000),
+(4,'Joe','HR',55000),
+(5,'Sam','Finance',70000);
+SELECT
+    EmployeeID,
+    EmployeeName,
+    Department,
+    Salary,
+    RANK() OVER (ORDER BY Salary DESC) AS SalaryRank,
+    DENSE_RANK() OVER (ORDER BY Salary DESC) AS DenseRank,
+    ROW_NUMBER() OVER (ORDER BY Salary DESC) AS RowNum
+FROM Employees;
