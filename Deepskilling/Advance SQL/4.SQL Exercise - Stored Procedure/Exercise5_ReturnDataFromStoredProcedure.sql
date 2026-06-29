ae86336a3bc0b2fda499c4fccef931ca70630873
Prathyusha chaluvadi <231fa04e60@gmail.com>
2026-06-29 12:00:38 +0530

    Create Exercise5_ReturnDataFromStoredProcedure.sql

---
+++ Deepskilling/Advance SQL/4.SQL Exercise - Stored Procedure/Exercise5_ReturnDataFromStoredProcedure.sql
@@ -0,0 +1,16 @@
+DROP PROCEDURE IF EXISTS GetEmployeeById;
+
+DELIMITER $$
+
+CREATE PROCEDURE GetEmployeeById(
+    IN emp_id INT
+)
+BEGIN
+    SELECT *
+    FROM EmployeeDetails
+    WHERE EmployeeID = emp_id;
+END $$
+
+DELIMITER ;
+
+CALL GetEmployeeById(1);
