bc77c18c18df5a242dbe2663bdd5f8d16523df6a
Prathyusha chaluvadi <231fa04e60@gmail.com>
2026-06-29 12:03:44 +0530

    Create Exercise7_ReturnDataFromScalarFunction.sql

---
+++ Deepskilling/Advance SQL/5.SQL Exercise - Functions/Exercise7_ReturnDataFromScalarFunction.sql
@@ -0,0 +1,21 @@
+DROP FUNCTION IF EXISTS GetAnnualSalary;
+
+DELIMITER $$
+
+CREATE FUNCTION GetAnnualSalary(
+    monthly_salary DECIMAL(10,2)
+)
+RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+    RETURN monthly_salary * 12;
+END $$
+
+DELIMITER ;
+
+SELECT
+    EmployeeID,
+    EmployeeName,
+    Salary,
+    GetAnnualSalary(Salary) AS AnnualSalary
+FROM EmployeeDetails;
