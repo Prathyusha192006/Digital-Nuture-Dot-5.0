bc328190931729a56dcaef6e848c0600bf637fd4
Prathyusha chaluvadi <231fa04e60@gmail.com>
2026-06-29 11:59:05 +0530

    Create Exercise1_CreateStoredProcedure.sql

---
+++ Deepskilling/Advance SQL/4.SQL Exercise - Stored Procedure/Exercise1_CreateStoredProcedure.sql
@@ -0,0 +1,19 @@
+USE CognizantSQL;
+CREATE TABLE EmployeeDetails (
+    EmployeeID INT PRIMARY KEY,
+    EmployeeName VARCHAR(50),
+    Salary DECIMAL(10,2)
+);
+INSERT INTO EmployeeDetails VALUES
+(1,'Prathyusha',50000),
+(2,'John',60000);
+DELIMITER $$
+
+CREATE PROCEDURE GetEmployees()
+BEGIN
+    SELECT * FROM EmployeeDetails;
+END $$
+
+DELIMITER ;
+
+CALL GetEmployees();
